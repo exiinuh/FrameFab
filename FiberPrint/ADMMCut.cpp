a35b18bfb63b9cc797001c7c576e23f124e8c525
Yijiang Huang <duckie@mail.ustc.edu.cn>
2016-05-05 09:13:34 +0800

    fix #38 finally.

--- FiberPrint/ADMMCut.cpp
+++ FiberPrint/ADMMCut.cpp
@@ -101,6 +101,7 @@ void ADMMCut::MakeLayers()
 		VX x_prev;
 		VX D_prev;
 
+		res_energy.clear();
 		/* Output energy list for reweighting process in a single
 		graph cut problem, energy.size() = number of reweighting
 		process performed.
@@ -474,7 +475,7 @@ void ADMMCut::CreateC()
 		int dual_v = ptr_dualgraph_->v(i);
 
 		double tmp_range = ptr_dualgraph_->Weight(i);
-		double tmp_height = exp(-3 * tmp_range * tmp_range);
+		double tmp_height = exp(-6 * tmp_range * tmp_range);
 
 		C_list.push_back(Triplet<double>(i, i, pow(tmp_height, 2) * r_(dual_u, dual_v)));
 	}
@@ -484,36 +485,36 @@ void ADMMCut::CreateC()
 	H1_ = SpMat(Nd_, Nd_);
 	H1_ = A_.transpose() * C_ * A_;
 
-	string path = "C:/Users/DELL/Desktop/result";
-	char cut_id[30];
-	sprintf(cut_id, "%d", cut_round_);
-	char reweight[30];
-	sprintf(reweight, "%d", reweight_round_);
+	//string path = "C:/Users/DELL/Desktop/result";
+	//char cut_id[30];
+	//sprintf(cut_id, "%d", cut_round_);
+	//char reweight[30];
+	//sprintf(reweight, "%d", reweight_round_);
 
-	string file = path + "/" + "H1_C_" + cut_id + "_" + reweight + ".txt";
-	FILE *fp = fopen(file.c_str(), "w");
-	for (int i = 0; i < Nd_; i++)
-	{
-		for (int j = 0; j < Nd_; j++)
-		{
-			fprintf(fp, "%lf ", H1_.coeff(i, j));
-		}
-		fprintf(fp, "\n");
-	}
-	fclose(fp);
+	//string file = path + "/" + "H1_C_" + cut_id + "_" + reweight + ".txt";
+	//FILE *fp = fopen(file.c_str(), "w");
+	//for (int i = 0; i < Nd_; i++)
+	//{
+	//	for (int j = 0; j < Nd_; j++)
+	//	{
+	//		fprintf(fp, "%lf ", H1_.coeff(i, j));
+	//	}
+	//	fprintf(fp, "\n");
+	//}
+	//fclose(fp);
 
-	string file2 = path + "/" + "H1_C_r_" + cut_id + "_" + reweight + ".txt";
-	FILE *fp1 = fopen(file2.c_str(), "w");
-	for (int i = 0; i < Nd_; i++)
-	{
-		for (int j = 0; j < Nd_; j++)
-		{
-			fprintf(fp1, "%lf ", r_(i, j));
-		}
-		fprintf(fp1, "\n");
-	}
+	//string file2 = path + "/" + "H1_C_r_" + cut_id + "_" + reweight + ".txt";
+	//FILE *fp1 = fopen(file2.c_str(), "w");
+	//for (int i = 0; i < Nd_; i++)
+	//{
+	//	for (int j = 0; j < Nd_; j++)
+	//	{
+	//		fprintf(fp1, "%lf ", r_(i, j));
+	//	}
+	//	fprintf(fp1, "\n");
+	//}
 
-	fclose(fp1);
+	//fclose(fp1);
 #else
 	C_.resize(2 * Md_, 2 * Md_);
 	vector<Triplet<double>> C_list;
@@ -532,7 +533,7 @@ void ADMMCut::CreateC()
 		int v = ptr_dualgraph_->e_orig_id(dual_v) / 2;
 
 		double tmp_range = ptr_dualgraph_->Weight(i);
-		double tmp_height = exp(-3 * tmp_range * tmp_range);
+		double tmp_height = exp(-6 * tmp_range * tmp_range);
 
 		/*double Wuv = col_weight_.coeff(u, v) * tmp_height;
 		double Wvu = col_weight_.coeff(v, u) * tmp_height;*/
